b7ec183ce22715a28b1a9f54b869d2778a288671
Nixon <kiplangatnixon28@gmail.com>
2024-08-08 17:41:07 +0300

    works with props

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,21 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
+
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        name={user.name} 
+        color={user.color} 
+        city={user.city} 
+      />
+      <About 
+        bio={user.bio} 
+        github={user.links.github} 
+        linkedin={user.links.linkedin} 
+      />
     </div>
   );
 }
